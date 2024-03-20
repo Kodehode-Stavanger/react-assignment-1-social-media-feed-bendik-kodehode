9f2e5ef22199b3b5fa0851d4bc8a048a42fd7b40
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-03-20 20:39:54 +0100

    Reworked AddPost component. Now displays error if no user is selected.

--- src/AddPost/AddPost.jsx
+++ src/AddPost/AddPost.jsx
@@ -1,25 +1,35 @@
+import { useState } from "react";
 import style from "./AddPost.module.css";
 
 function AddPost({setPosts, selectedUser}) {
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        const formData = new FormData(e.target);
-        const newPost = {
-            username: selectedUser,
-            content: formData.get("content"),
-            timestamp: new Date().toISOString().replace(/\.\d{3}Z$/, "Z"), // Removes milliseconds.
-            likes: 1
-        };
-        setPosts(prevPosts => [newPost, ...prevPosts]);
+        if (selectedUser.length !== 0) {
+            const newPost = {
+                username: selectedUser,
+                content: message,
+                timestamp: new Date().toISOString().replace(/\.\d{3}Z$/, "Z"), // Removes milliseconds.
+                likes: 1
+            };
+            setPosts(prevPosts => [newPost, ...prevPosts]);
+            setError("");
+            setMessage("");
+        } else {
+            setError("Please select a user.");
+        }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <textarea name="content" placeholder="What's on your mind?" autoComplete="off" required></textarea>
+        <div className={style.container}>
+            <textarea id="textField" name="textField" value={message} autoComplete="off" placeholder="What's on your mind?" required onChange={(e) => setMessage(e.target.value)}></textarea>
             <div className={style.btnContainer}>
-                <button type="submit">Post</button>
+                <button type="submit" onClick={handleSubmit}>Post</button>
             </div>
-        </form>
+            {error && <div className={style.error}>{error}</div>}
+        </div>
     )
 }
 
